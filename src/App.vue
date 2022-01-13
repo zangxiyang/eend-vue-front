b6697c338da95efdbfb031c4fc460e87b16fe219
Seale <1801157108@qq.com>
2022-01-13 15:02:57 +0800

    [env] 加入环境变量

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-//
-</script>
-
 <template>
   <div class="flex justify-center">
     <img alt="Vue logo" src="./assets/logo.png" />
   </div>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+
   <router-view/>
 </template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
 
 <style>
 #app {
